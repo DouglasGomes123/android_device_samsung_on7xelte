bc54608ec19caace79261bd6bd305fa238d8362c
Douglas Gomes <DouglasGomes123@users.noreply.github.com>
2019-03-24 12:56:17 -0300

    Update lineage_on7xelte.mk

--- lineage_on7xelte.mk
+++ lineage_on7xelte.mk
@@ -23,6 +23,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_BOOT_ANIMATION_RES := 1080x1920
+
 # Device identifier
 PRODUCT_NAME := lineage_on7xelte
 PRODUCT_DEVICE := on7xelte
